6531e18a85cf042c16cd4f5ac0ea6ed3ddf1150d
mohan_2231 <mahan.singh.tomar1034@gmail.com>
2024-02-22 09:35:38 +0530

    add user subscription detail

--- client/src/pages/subscriptionPlan/SubscriptionPlan.jsx
+++ client/src/pages/subscriptionPlan/SubscriptionPlan.jsx
@@ -1,13 +1,24 @@
-// SubscriptionPlans.js
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SubscriptionPlan.css';
 import { axiosClient } from '../../utils/axiosClient';
-import { loadStripe } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js';
 
 function SubscriptionPlans() {
-
     const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await axiosClient.get('/auth/update-user');
+                setUser(response.data);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+        fetchUser();
+    }, []);
+
     const handleCheckout = async (plan) => {
         try {
             setIsLoading(true);
@@ -20,7 +31,11 @@ function SubscriptionPlans() {
 
                 if (error) {
                     console.error('Error redirecting to Stripe Checkout:', error);
-                } else { await axiosClient.post('/update-subscription', { plan }) }
+                } else {
+                    await axiosClient.post('/update-subscription', { plan });
+                    // After updating the subscription, refetch the user data
+                    fetchUser();
+                }
             } else {
                 console.error('No sessionId received from backend');
             }
@@ -34,24 +49,39 @@ function SubscriptionPlans() {
     return (
         <div className="subscription-plans-container">
             <h2 style={{ textAlign: "center" }}>Choose a Subscription Plan</h2>
-            <div className="p-grid">
-                <div className="card">
-                    <h3>Free Plan</h3>
-                    <p>Can post only 1 question a day</p>
-                </div>
-                <div className="card" >
-                    <h3>Silver Plan</h3>
-                    <p>₹100/month</p>
-                    <p>Can post 5 questions a day</p>
-                    <button className="s-button" disabled={isLoading} onClick={() => handleCheckout('silver')}>Subscribe</button>
-                </div>
-                <div className="card">
-                    <h3>Gold Plan</h3>
-                    <p>₹1000/month</p>
-                    <p>Unlimited questions</p>
-                    <button className="s-button" disabled={isLoading} onClick={() => handleCheckout('gold')}>Subscribe</button>
-                </div>
-            </div>
+            {user && (
+                <>
+                    <div className='currentplan'>
+                        <div><strong><h3 style={{ display: 'inline' }}>Current Plan:</h3></strong> {user.subscription.plan}</div>
+
+                        {user.subscription.subscriptionEnd && (
+                            <div>
+                                <strong><h3 style={{ display: 'inline' }}>Validity: </h3></strong> {new Date(user.subscription.subscriptionEnd).toLocaleDateString()}
+                            </div>
+                        )}
+                    </div>
+
+
+                    <div className="p-grid">
+                        <div className="card">
+                            <h3>Free Plan</h3>
+                            <p>Can post only 1 question a day</p>
+                        </div>
+                        <div className="card" >
+                            <h3>Silver Plan</h3>
+                            <p>₹100/month</p>
+                            <p>Can post 5 questions a day</p>
+                            <button className="s-button" disabled={isLoading || user?.subscription.plan === 'silver'} onClick={() => handleCheckout('silver')}>Subscribe</button>
+                        </div>
+                        <div className="card">
+                            <h3>Gold Plan</h3>
+                            <p>₹1000/month</p>
+                            <p>Unlimited questions</p>
+                            <button className="s-button" disabled={isLoading || user?.subscription.plan === 'gold'} onClick={() => handleCheckout('gold')}>Subscribe</button>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
